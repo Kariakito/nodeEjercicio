e3bdb734505a9eb434d1b1679e91d768742d9040
kariakito <pilarmaestre@gmail.com>
2021-06-16 17:37:54 +0200

    q vaya a github

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = requiere('chalk');
+
+console.log(chalk.blue('Hello Salvo!'));
+console.log(chalk.red('Hello Alex!'));
+console.log(chalk.yellow('Hello Pallavi!'));
+console.log(chalk.green('Hello Nat!'));
